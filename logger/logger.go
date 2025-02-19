460f5e4b390ea06e2a000779889f97ddb3d820fc
alexesp <aleizaes@hotmail.com>
2025-02-19 11:40:53 +0100

    crear kafca config

---
+++ logger/logger.go
@@ -0,0 +1,15 @@
+package logger
+
+import (
+	"os"
+
+	"github.com/alexesp/Go_Pizza_Shop_Eda_Kafka/order_service/utils/"
+)
+
+func Log(message any) {
+	isLoggedEnabled := os.Getenv("log")
+
+	if isLoggedEnabled != "" {
+		utils.AppendToFile("log.txt", message.(string))
+	}
+}
--- order_service/config/env_config.go
+++ order_service/config/env_config.go
@@ -1,8 +1,12 @@
 package config
 
+import (
+	"log"
+
+	"github.com/joho/godotenv"
+)
 
 var env ConfigDto
-
 
 var ConfigDto struct {
 	port                string
@@ -14,22 +18,36 @@ var ConfigDto struct {
 	kafka_group_id      string
 }
 
-func init(){
-	if env.port == ""{
+func init() {
+	if env.port == "" {
 		ConfigEnv()
 	}
 }
 
-func ConfigEnv(){
+func ConfigEnv() {
 	LoadEnvVariable()
 	env = ConfigDto{
-		port                os.Getenv("PORT"),
-		database_url        os.Getenv("MONGO_DB_URL"),
-		database_name       os.Getenv("MONGO_DB_NAME"),
-		kafka_host          os.Getenv("KAFKA_HOST"),
-		kafka_port          os.Getenv("KAFKA_PORT"),
-		kafka_default_topic os.Getenv("KAFKA_DEFAULT_TOPIC"),
-		kafka_group_id      os.Getenv("KAFKA_GROUP_ID"),
+	port                os.Getenv("PORT"),
+	database_url        os.Getenv("MONGO_DB_URL"),
+	database_name       os.Getenv("MONGO_DB_NAME"),
+	kafka_host          os.Getenv("KAFKA_HOST"),
+	kafka_port          os.Getenv("KAFKA_PORT"),
+	kafka_default_topic os.Getenv("KAFKA_DEFAULT_TOPIC"),
+	kafka_group_id      os.Getenv("KAFKA_GROUP_ID"),
+
 	}
+}
+
+
+
+func LoadEnvVariable(){
 	
+	if _, err, os.Stat(".env"); err != nil{
+		err := godotenv.Load()
+		if err != nil{
+			log.Fatalf("error loading .env file: %v", err)
+		}
+	}else {
+		logge
+	}
 }
