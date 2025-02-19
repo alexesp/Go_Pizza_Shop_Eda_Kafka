28b7bfd9531a593ed8ebbe3634eb0e52d31b962b
alexesp <aleizaes@hotmail.com>
2025-02-19 10:28:40 +0100

    crear kafca config

---
+++ order_service/config/env_config.go
@@ -0,0 +1,35 @@
+package config
+
+
+var env ConfigDto
+
+
+var ConfigDto struct {
+	port                string
+	database_url        string
+	database_name       string
+	kafka_host          string
+	kafka_port          string
+	kafka_default_topic string
+	kafka_group_id      string
+}
+
+func init(){
+	if env.port == ""{
+		ConfigEnv()
+	}
+}
+
+func ConfigEnv(){
+	LoadEnvVariable()
+	env = ConfigDto{
+		port                os.Getenv("PORT"),
+		database_url        os.Getenv("MONGO_DB_URL"),
+		database_name       os.Getenv("MONGO_DB_NAME"),
+		kafka_host          os.Getenv("KAFKA_HOST"),
+		kafka_port          os.Getenv("KAFKA_PORT"),
+		kafka_default_topic os.Getenv("KAFKA_DEFAULT_TOPIC"),
+		kafka_group_id      os.Getenv("KAFKA_GROUP_ID"),
+	}
+	
+}
